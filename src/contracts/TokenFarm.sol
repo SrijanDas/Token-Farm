6c45ad5327a20622f55468db72fc3aea5334e882
unknown <srijandas18.sd@gmail.com>
2022-04-05 11:51:17 +0530

    created client side ui

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -41,7 +41,23 @@ contract TokenFarm {
         hasStaked[msg.sender] = true;
     }
 
-    // Unstaking token
+    // Unstaking Tokens (Withdraw)
+    function unstakeTokens() public {
+        // Fetch staking balance
+        uint256 balance = stakingBalance[msg.sender];
+
+        // Require amount greater than 0
+        require(balance > 0, "staking balance cannot be 0");
+
+        // Transfer Mock Dai tokens to this contract for staking
+        daiToken.transfer(msg.sender, balance);
+
+        // Reset staking balance
+        stakingBalance[msg.sender] = 0;
+
+        // Update staking status
+        isStaking[msg.sender] = false;
+    }
 
     // Issuing tokens
     function issueTokens() public {
